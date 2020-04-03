27ce871e711b2c3ca82014f87452e90eb438d1ba
natelloyd1 <nathanaellloyd@live.com>
2020-04-03 12:08:10 +0100

    add playerList file

---
+++ src/components/playerList.js
@@ -0,0 +1,21 @@
+// import React, { Component } from "react";
+
+// const Players = ({ name }) => <li>{name}</li>
+
+// const playerList = ({names, Players}) => (
+
+
+//         <div className="App">
+//             <ul className="namesList">
+//                 {names.map((name) => (
+//                     <Players
+//                         name={name}
+//                         // Prevent duplicate keys by appending index:
+//                         key={name}
+//                     />
+//                 ))}
+//             </ul>
+//         </div>
+// );
+
+// export default playerList;
